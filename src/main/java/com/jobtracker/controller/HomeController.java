953d58dd2f599cc30a3c48432f0c6887eb4dc1ad
PAVANNAYAK45 <pavannayak1205@gmail.com>
2026-03-24 10:20:37 +0530

    add home endpoint

---
+++ src/main/java/com/jobtracker/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.jobtracker.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "JobTracker API is running";
+    }
+}
